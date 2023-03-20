82cb6b89e184cfd7b3fdb7b22e3e5cc8ee4db25a
shuozhi <shuozhi.zp@wayxtech.com>
2023-03-20 13:58:31 +0800

    feature: 计算并打印面积ok

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,12 @@ fn test_traffic_light_time() {
 
 
 /// 实现一个函数，为u32类型的整数集合求和，参数类型为 &[u32]，返回类型为Option，溢出时返回None
-const U32MAX: u32 = 0xFFFFFFFFu32;
 
 fn sum(array: &[u32]) -> Option<u32> {
     let mut result = 0u32;
     for item in array {
         // 如果剩余可累加量,< 下一个累加值，则必定溢出
-        if U32MAX - result < *item {
+        if u32::MAX - result < *item {
             return None;
         }
         result += *item;
@@ -61,11 +60,35 @@ fn call_sum(array: &[u32]) -> Option<u32> {
 fn test_sum() {
     let to_sum_normal = [1u32, 2u32, 4u32];
     assert_eq!(call_sum(&to_sum_normal), Some(7u32));
-    let to_sum_overflow = [1u32, 2u32, 4u32, U32MAX];
+    let to_sum_overflow = [1u32, 2u32, 4u32, u32::MAX];
     assert_eq!(call_sum(&to_sum_overflow), None)
 }
 
 /// 实现一个打印图形面积的函数，它接收一个可以计算面积的类型作为参数，比如圆形，三角形，正方形，需要用到泛型和泛型约束
+trait AreaCalculable {
+    fn calculate_area(&self) -> f32;
+}
+
+// 计算并打印面积
+fn calculate_and_print_area<T: AreaCalculable>(shape: T) {
+    let area = shape.calculate_area();
+    println!("面积:{}", area);
+}
+
+struct Cycle {
+    radius: f32,
+}
+
+impl AreaCalculable for Cycle {
+    fn calculate_area(&self) -> f32 {
+        3.14 * self.radius * self.radius
+    }
+}
+
+#[test]
+fn test_area_calculate() {
+    calculate_and_print_area(Cycle { radius: 2.0f32 })
+}
 
 fn main() {
     println!("Hello, world!");
